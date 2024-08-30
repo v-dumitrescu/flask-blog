97f5d0c766d52fab2263f66d2c95486fec2a1320
Valentin Dumitrescu <val3ntin.dumitrescu@gmail.com>
2024-08-30 17:28:49 +0300

    Set forms.py for register/login classes, register.html and description for index, about pages

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegisterForm
+
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "ed13ff47228583ac38fca4ee4f91fb19"
 
 @app.route("/")
 @app.route("/home")
@@ -9,6 +13,11 @@ def home():
 @app.route("/about")
 def about():
   return render_template("about.html")
+
+@app.route("/register")
+def register():
+  form = RegisterForm()
+  return render_template("register.html", form=form)
 
 if __name__ == "__main__":
   app.run(debug=True)
